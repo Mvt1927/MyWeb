75bde6aaad724551e2130fe20f6c8f2a9455a72b
Mvt1927 <viettheog4@gmail.com>
2021-08-21 08:58:50 +0700

    My Web

--- GithubPush.bat
+++ GithubPush.bat
@@ -1,5 +1,5 @@
 git add .
 git commit -m "first commit"
-git remote add origin https://github.com/viettel3g1000/Lab1_login.git
+git remote add github https://github.com/viettel3g1000/MyWeb.git
 git branch -M main
 git push -u origin main
